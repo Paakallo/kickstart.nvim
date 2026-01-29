2c11782b9330823eac57294038b373034a6bab57
Sergiusz Pyskowacki <paviveerar@gmail.com>
2026-01-29 17:24:58 +0100

    spaces instead of tabs

--- init.lua
+++ init.lua
@@ -244,6 +244,12 @@ rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 
+-- Spaces instead of tab
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- LSP
 vim.lsp.enable('clangd', true)
 
