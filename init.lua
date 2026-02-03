521166defb7eb736a5a26a6c2898c80468b7906f
Sergiusz Pyskowacki <paviveerar@gmail.com>
2026-02-03 18:54:57 +0100

    working c/c++ debugger

--- init.lua
+++ init.lua
@@ -998,6 +998,7 @@ require('lazy').setup({
   -- you can continue same window with `<space>sr` which resumes last telescope search
 
   -- DAP plugin
+  -- DAP plugin
   {
     'mfussenegger/nvim-dap',
     dependencies = {
@@ -1005,6 +1006,9 @@ require('lazy').setup({
       'jay-babu/mason-nvim-dap.nvim',
     },
     config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
       require('mason-nvim-dap').setup {
         ensure_installed = { 'cpptools' },
         automatic_installation = true,
@@ -1015,7 +1019,19 @@ require('lazy').setup({
         },
       }
 
-      local dap = require 'dap'
+      -- Basic debugging keymaps
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      vim.keymap.set('n', '<leader>B', function()
+        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end, { desc = 'Debug: Set Breakpoint' })
+
+      -- Toggle to see last session result. Without this, you can't see the UI if you close it!
+      vim.keymap.set('n', '<F7>', ui.toggle, { desc = 'Debug: See last session result.' })
+
       dap.configurations.cpp = {
         {
           name = 'Launch file',
@@ -1028,10 +1044,11 @@ require('lazy').setup({
           stopAtEntry = true,
         },
       }
+
+      -- Apply the same config to C files
       dap.configurations.c = dap.configurations.cpp
     end,
   },
-
   -- DAP GUI
   {
     'rcarriga/nvim-dap-ui',
