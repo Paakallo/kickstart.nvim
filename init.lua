a795521b8b58d4fbbacfa01c058130709bd5f108
Paakallo <paviveerar@gmail.com>
2026-04-19 20:51:14 +0200

    turn on pylsp

--- init.lua
+++ init.lua
@@ -913,7 +913,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        -- pylsp = {},
+        pylsp = {},
         cmake = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
